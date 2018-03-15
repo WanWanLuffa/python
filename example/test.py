aecd8ae09e7f3efa9a395df920ef7640baf54564
WanWanLuffa <15801095153@163.com>
2018-03-15 15:53:04 +0800

    modify file

---
+++ example/test.py
@@ -0,0 +1,13 @@
+
+
+def add(*num):
+    x = 0
+    for i in num:
+        x += i
+    return x
+
+
+a = 'aasdf2621smsfsdfr3idd4bgs7Dsf9eAF'
+print a.swapcase()
+
+print add(1, 2, 3, 4, 5, 6, 8, 3455, 234)
